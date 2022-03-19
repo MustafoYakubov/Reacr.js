92a2819398367bc05d5541f434a97eda22cc817d
mustafa6143170@gmail.com <91618477+DushanovShamshod@users.noreply.github.com>
2022-03-19 20:52:48 +0500

    push

--- project_empty/src/App.jsx
+++ project_empty/src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Discover from "./components/Discover/Discover";
 import HowWork from "./components/HowWork/HowWork";
+import Notification from "./components/Notification/Notification";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Discover />} />
           <Route path="/how-it-works" element={<HowWork />} />
+          <Route path="/Notification" element={<Notification />} />
         </Routes>
       </BrowserRouter>
     </>
--- project_empty/src/layout/Navbar/Navbar.jsx
+++ project_empty/src/layout/Navbar/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "../Navbar/Navbar.scss";
 import logo from "../../assets/icons/symbol.svg";
-import styles from "../Navbar/Navbar.module.scss";
+import search from "../../assets/icons/search.svg";
+import Notification from "../../assets/icons/Notification.svg";
+import mobileNavIcon from "../../assets/icons/mobile-icon-nav.svg";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light ">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          <img className={styles.logo} src={logo} alt="" />
-        </Link>
+        <NavLink className="navbar-brand" to="/">
+          <img className="logo" src={logo} alt="logo" />
+          <div className="position">
+            <span className="crypto"> crypter</span>
+          </div>
+        </NavLink>
         <button
           className="navbar-toggler"
           type="button"
@@ -18,31 +24,41 @@ export default function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span >
+            <img src={mobileNavIcon} alt="mobile-nav" />
+          </span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link " aria-current="page" to="/">
+              <NavLink className="nav-NavLink  " aria-current="page" to="/">
                 Discover
-              </Link>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/how-it-works">
+              <NavLink className="nav-NavLink" to="/how-it-works">
                 How it Work
-              </Link>
+              </NavLink>
             </li>
           </ul>
-          <form className="d-flex">
+          <form className="d-flex forms">
             <input
               className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
             />
-            <a href=""> icon ring </a>
-            <button>upload</button>
-            <button>Connect Wallet</button>
+            <img className="search-logo" src={search} alt="search-logo" />
+            <NavLink className="notification" to="/Notification">
+              <img className="noti-logo" src={Notification} alt="Notification" />
+            </NavLink>
+            <button type="button" className="upload">
+              upload
+            </button>
+            <button type="button" className="connect">
+            
+              <span className="connect-text"> Connect Wallet </span>
+            </button>
           </form>
         </div>
       </div>
