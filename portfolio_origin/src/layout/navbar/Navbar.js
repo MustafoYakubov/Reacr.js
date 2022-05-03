1f79f210afb26e6742a64d6b5aef1ab36f834b2b
mustafa6143170@gmail.com <91618477+DushanovShamshod@users.noreply.github.com>
2022-05-03 23:45:14 +0500

    push

--- portfolio_origin/src/layout/navbar/Navbar.js
+++ portfolio_origin/src/layout/navbar/Navbar.js
@@ -3,10 +3,16 @@ import logo from "../../assets/icons/logo.svg";
 import "./Navbar.scss";
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [isActiveBtnAnimate, setIsActiveBtnAnimate] = useState(false);
   const toggleBtn = () => setToggle((prev) => !prev);
   function scrollWin() {
     window.scrollBy(0, 900);
   }
+  const navBtnAnimate = () => {
+    setIsActiveBtnAnimate((prev) => !prev);
+    //     const navmen = document.getElementById("#navMenu");
+    //     navmen.classList.toggle("active");
+  };
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -14,17 +20,24 @@ const Navbar = () => {
           <a className="navbar-brand" href="#">
             <img src={logo} alt="logo" />
           </a>
-          <button
-            className="navbar-toggler"
+          <div
+            className={`navMenu navbar-toggler ${
+              isActiveBtnAnimate ? "active" : ""
+            } `}
+            // className=" "
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            onClick={navBtnAnimate}
           >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+            {/* <span className="navbar-toggler-icon"></span> */}
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
               <a
