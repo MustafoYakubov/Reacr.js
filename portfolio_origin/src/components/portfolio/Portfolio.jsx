dc0fc08ea581552d81325960f32b9e25f5bead68
mustafa6143170@gmail.com <91618477+DushanovShamshod@users.noreply.github.com>
2022-05-04 01:08:25 +0500

    push

--- portfolio_origin/src/components/portfolio/Portfolio.jsx
+++ portfolio_origin/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,48 @@
 import React from "react";
 import "./portfolio.scss";
+import gameRisk from "../../assets/img/";
 const Portfolio = () => {
   return (
     <>
-      <div>Portfolio</div>
+      <section className="portfolio">
+        <div className="row">
+          <div className=" main_img col-8">
+            <div className="card">
+              <div className="card-body">
+                <img className="w-100 main_portfolio_img" src={gameRisk} alt="gameRisk " />
+              </div>
+              <div className="card-text">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Accusamus in sequi iusto cum officia, minus ad recusandae ea
+                  id odit quibusdam laudantium eligendi dicta praesentium omnis
+                  vitae modi, unde nam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="card">
+              <div className="card-body"><h1>aa</h1></div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="card">
+              <div className="card-body"></div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="card">
+              <div className="card-body"></div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="card">
+              <div className="card-body"></div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
