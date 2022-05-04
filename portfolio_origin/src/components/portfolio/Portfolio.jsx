6157ed5bb637be9711b015941b915d208c0c36ce
mustafa6143170@gmail.com <91618477+DushanovShamshod@users.noreply.github.com>
2022-05-04 20:20:22 +0500

    push

--- portfolio_origin/src/components/portfolio/Portfolio.jsx
+++ portfolio_origin/src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./portfolio.scss";
-import gameRisk from "../../assets/img/";
+import gameRisk from "../../assets/img/game.png";
+import woman from "../../assets/img/wom.jpg";
 const Portfolio = () => {
   return (
     <>
       <section className="portfolio">
         <div className="row">
-          <div className=" main_img col-8">
+          <div className=" main_img col-4">
             <div className="card">
               <div className="card-body">
-                <img className="w-100 main_portfolio_img" src={gameRisk} alt="gameRisk " />
+                <img className="w-100 main_portfolio_img" src={woman} alt="gameRisk " />
               </div>
               <div className="card-text">
                 <p>
@@ -21,12 +22,22 @@ const Portfolio = () => {
               </div>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-4">
             <div className="card">
-              <div className="card-body"><h1>aa</h1></div>
+              <div className="card-body"><h1>
+                <img className="w-100" src={gameRisk} alt="" /></h1></div>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-4">
+            <div className="card">
+              <div className="card-body">
+                <img className="w-100 wom" src={woman} alt="" />
+                <div className="overlay"></div>
+                <button className="btn-link"> <p>click</p></button>
+              </div>
+            </div>
+          </div>
+          {/* <div className="col-6">
             <div className="card">
               <div className="card-body"></div>
             </div>
@@ -35,12 +46,7 @@ const Portfolio = () => {
             <div className="card">
               <div className="card-body"></div>
             </div>
-          </div>
-          <div className="col-6">
-            <div className="card">
-              <div className="card-body"></div>
-            </div>
-          </div>
+          </div> */}
         </div>
       </section>
     </>
