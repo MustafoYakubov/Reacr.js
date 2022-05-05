275e74baaeab68618e9adc504a95ac2297054865
mustafa6143170@gmail.com <91618477+DushanovShamshod@users.noreply.github.com>
2022-05-05 16:05:57 +0500

    push

--- portfolio_origin/src/components/portfolio/Portfolio.jsx
+++ portfolio_origin/src/components/portfolio/Portfolio.jsx
@@ -5,19 +5,15 @@ import gameNumber from "../../assets/img/gameNumber.png";
 import nftStore from "../../assets/img/nft.jpg";
 import pumaStore from "../../assets/img/pum.jpg";
 import samsungStore from "../../assets/img/samsungStore.jpg";
-import react from "../../assets/icons/react.png";
-import html from "../../assets/icons/html.png";
-import sass from "../../assets/icons/sass.png";
-import bootstrap from "../../assets/icons/bootstrap.png";
+// import react from "../../assets/icons/react.png";
+// import html from "../../assets/icons/html.png";
+// import sass from "../../assets/icons/sass.png";
+// import bootstrap from "../../assets/icons/bootstrap.png";
 const Portfolio = () => {
   return (
     <>
       <section className="portfolio">
         <div className="row">
-          <div className="col-4">
-            <img className="w-100" src={pumaStore} alt="" />
-            <div className="card-footer"></div>
-          </div>
           <div className="col-4">
             <img className="w-100" src={pumaStore} alt="" />
             <div className="card-footer">
@@ -28,14 +24,90 @@ const Portfolio = () => {
                 <span>Bootstrap</span>
               </div>
               <div className="card_footer_btns">
-                <div className="btn-yellow"><a>view code</a></div>
-                <div className="btn-yellow"><a>view  web-site</a></div>
+                <div className="btn-yellow">
+                  <a to="https://github.com">view code</a>
+                </div>
+                <div className="btn-yellow">
+                  <a>view web-site</a>
+                </div>
               </div>
             </div>
           </div>
           <div className="col-4">
-            <img className="w-100" src={pumaStore} alt="" />
-            <div className="card-footer"></div>
+            <img className="w-100" src={samsungStore} alt="" />
+            <div className="card-footer">
+              <div className="card_footer_txt">
+                <span>JavaScript</span>
+                <span>Html</span>
+                <span>CSS</span>
+                <span>Bootstrap</span>
+              </div>
+              <div className="card_footer_btns">
+                <div className="btn-yellow">
+                  <a to="https://github.com">view code</a>
+                </div>
+                <div className="btn-yellow">
+                  <a>view web-site</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <img className="w-100" src={nftStore} alt="" />
+            <div className="card-footer">
+              <div className="card_footer_txt">
+                <span>JavaScript</span>
+                <span>Html</span>
+                <span>CSS</span>
+                <span>Bootstrap</span>
+              </div>
+              <div className="card_footer_btns">
+                <div className="btn-yellow">
+                  <a to="https://github.com">view code</a>
+                </div>
+                <div className="btn-yellow">
+                  <a>view web-site</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-5 offset-1 mt-5">
+            <img className="w-100" src={gameRisk} alt="" />
+            <div className="card-footer">
+              <div className="card_footer_txt">
+                <span>JavaScript</span>
+                <span>Html</span>
+                <span>CSS</span>
+                <span>Bootstrap</span>
+              </div>
+              <div className="card_footer_btns">
+                <div className="btn-yellow">
+                  <a to="https://github.com">view code</a>
+                </div>
+                <div className="btn-yellow">
+                  <a>view web-site</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-5 mt-5">
+            <img className="w-100" src={gameNumber} alt="" />
+            <div className="card-footer">
+              <div className="card_footer_txt">
+                <span>JavaScript</span>
+                <span>Html</span>
+                <span>CSS</span>
+                <span>Bootstrap</span>
+              </div>
+              <div className="card_footer_btns">
+                <div className="btn-yellow">
+                  <a to="https://github.com">view code</a>
+                </div>
+                <div className="btn-yellow">
+                  <a>view web-site</a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
