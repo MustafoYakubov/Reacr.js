bdf39bf028e94910b39c708a77d5d64c74b37490
mustafa6143170@gmail.com <91618477+DushanovShamshod@users.noreply.github.com>
2022-05-05 15:29:34 +0500

    push

--- portfolio_origin/src/components/portfolio/Portfolio.jsx
+++ portfolio_origin/src/components/portfolio/Portfolio.jsx
@@ -5,73 +5,43 @@ import gameNumber from "../../assets/img/gameNumber.png";
 import nftStore from "../../assets/img/nft.jpg";
 import pumaStore from "../../assets/img/pum.jpg";
 import samsungStore from "../../assets/img/samsungStore.jpg";
+import react from "../../assets/icons/react.png";
+import html from "../../assets/icons/html.png";
+import sass from "../../assets/icons/sass.png";
+import bootstrap from "../../assets/icons/bootstrap.png";
 const Portfolio = () => {
   return (
     <>
       <section className="portfolio">
         <div className="row">
-          <div className="col-xl-4 col-sm-10">
-            <div className="card">
-              <div className="card-body">
-                <img
-                  className="w-100 imgs_corners "
-                  src={nftStore}
-                  alt="gameRisk "
-                />
-                <div className="card-footer">
-                  <p>made with : <span> Bootstrap</span> </p>
-                </div>
+          <div className="col-4">
+            <img className="w-100" src={pumaStore} alt="" />
+            <div className="card-footer"></div>
+          </div>
+          <div className="col-4">
+            <img className="w-100" src={pumaStore} alt="" />
+            <div className="card-footer">
+              <div className="card_footer_txt">
+                <span>JavaScript</span>
+                <span>Html</span>
+                <span>CSS</span>
+                <span>Bootstrap</span>
+              </div>
+              <div className="card_footer_btns">
+                <div className="btn-yellow"><a>view code</a></div>
+                <div className="btn-yellow"><a>view  web-site</a></div>
               </div>
             </div>
           </div>
-          <div className="col-xl-4 col-sm-10">
-            <div className="card">
-              <div className="card-body">
-                <img
-                  className="w-100 web_games_img"
-                  src={gameNumber}
-                  alt="gameRisk"
-                />
-                <div className="card-footer"></div>
-              </div>
-            </div>
-            <div className="card mt-5">
-              <div className="card-body">
-                <img
-                  className="w-100 web_games_img "
-                  src={gameRisk}
-                  alt="gameRisk"
-                />
-                <div className="card-footer"></div>
-              </div>
-            </div>
-            <div className="card mt-5">
-              <div className="card-body">
-                <img
-                  className="w-100 web_games_img "
-                  src={samsungStore}
-                  alt="gameRisk"
-                />
-                <div className="card-footer"></div>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-sm-10">
-            <div className="card">
-              <div className="card-body">
-                <img
-                  className="w-100  imgs_corners"
-                  src={pumaStore}
-                  alt="pumaStore"
-                />
-                <div className="card-footer"></div>
-              </div>
-            </div>
+          <div className="col-4">
+            <img className="w-100" src={pumaStore} alt="" />
+            <div className="card-footer"></div>
           </div>
         </div>
       </section>
     </>
   );
 };
+// background-color: rgb(30, 31, 28) !important;
 
 export default Portfolio;
