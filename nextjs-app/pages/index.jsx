e08ef6311f165298577b25c4cadbb957c01a2356
mustafa6143170@gmail.com <91618477+DushanovShamshod@users.noreply.github.com>
2022-05-14 19:06:39 +0500

    push

---
+++ nextjs-app/pages/index.jsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next js</title>
+        <meta name="description" content="Next app" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <div className="homeContent">
+        <h1>Home</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+          eaque pariatur ipsa quibusdam quos assumenda dolore nisi laboriosam
+          soluta perferendis itaque possimus animi quod porro corporis omnis,
+          accusamus eum laborum.
+        </p>
+      </div>
+    </div>
+  );
+}
