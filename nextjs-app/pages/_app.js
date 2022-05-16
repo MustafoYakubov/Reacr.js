87fb4f1ba904ca8579856b936ebe3355148d2909
mustafa6143170@gmail.com <91618477+DushanovShamshod@users.noreply.github.com>
2022-05-16 19:07:55 +0500

    push

--- nextjs-app/pages/_app.js
+++ nextjs-app/pages/_app.js
@@ -1,7 +1,5 @@
 import "../styles/globals.css";
-import Layout from "../components/Layout";
-import Post from "./post";
-import Home from ".";
+
 const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 //   <Home />;
