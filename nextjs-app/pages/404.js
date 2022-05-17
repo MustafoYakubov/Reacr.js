042d446f258268bc6082190abcca9d0ee9111d11
mustafa6143170@gmail.com <91618477+DushanovShamshod@users.noreply.github.com>
2022-05-17 16:46:07 +0500

    push

--- nextjs-app/pages/404.js
+++ nextjs-app/pages/404.js
@@ -3,16 +3,17 @@ import { useRouter } from "next/router";
 import Heading from "../components/Heading";
 
 const Error = () => {
-  
-  useEffect(()=> {
-
-  },[])
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/"); 
+    }, 5000);
+  }, [router]);
   return (
     <div>
       Error 404
       <h1 style={{ marginBottom: "200px" }}>Something is going wrong </h1>
       <Heading text={"This page of error "} />
-
     </div>
   );
 };
