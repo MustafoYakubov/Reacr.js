f8b076aa45d7ca72e8701dc308c84a5cd0b5cf6f
Your Name <mustafa6143170@gmail.com>
2022-06-28 21:16:17 +0500

    push

--- gamestore/src/pages/homePage/HomePage.js
+++ gamestore/src/pages/homePage/HomePage.js
@@ -1,5 +1,7 @@
+import React from "react";
 import { GameItem } from "../../components/gameItem/GameItem";
 import "./HomePage.scss";
+
 const games = [
   {
     image: "/game-covers/forza_5.jpeg",
@@ -7,7 +9,7 @@ const games = [
     genres: ["Гонки", "Симулятор", "Открытый мир"],
     price: 2343,
     video: "https://www.youtube.com/embed/FYH9n37B7Yw",
-    id: 1,
+    id: Math.random(),
     description:
       "Вас ждёт бесконечный калейдоскоп приключений Horizon! Совершайте увлекательные поездки по невероятно красивому и самобытному миру Мексики за рулём величайших автомобилей в истории. Начните своё приключение Horizon уже сегодня, добавив игру в свой список желаний!",
   },
@@ -17,7 +19,7 @@ const games = [
     genres: ["Экшен", "Шутер", "Война"],
     video: "https://www.youtube.com/embed/ASzOzrB-a9E",
     price: 2433,
-    id: 2,
+    id: Math.random(),
     description:
       "Battlefield™ 2042 — это шутер от первого лица, в котором серия возвращается к легендарным масштабным сражениям. В будущем, где царит хаос, адаптируйтесь и процветайте на постоянно меняющихся полях боя благодаря своему отряду и арсеналу новейших технологий.",
   },
@@ -27,7 +29,7 @@ const games = [
     genres: ["Глубокий сюжет", "Протагонистка"],
     video: "https://www.youtube.com/embed/b6CkzwVAr0M",
     price: 3021,
-    id: 3,
+    id: Math.random(),
     description:
       "Алекс Чэнь от всех скрывает своё «проклятие» — сверхъестественную способность считывать сильные эмоции других и влиять на них. Но когда её брат погибает — якобы в результате несчастного случая, — Алекс использует её, чтобы узнать правду.",
   },
@@ -37,7 +39,7 @@ const games = [
     genres: ["Открытый мир", "Экшен"],
     video: "https://www.youtube.com/embed/QkkoHAzjnUs",
     price: 789,
-    id: 4,
+    id: Math.random(),
     description:
       "Grand Theft Auto V для PC позволяет игрокам исследовать знаменитый мир Лос-Сантоса и округа Блэйн в разрешении до 4k и выше с частотой 60 кадров в секунду.",
   },
@@ -47,7 +49,7 @@ const games = [
     video: "https://www.youtube.com/embed/6wlvYh0h63k",
     genres: ["Тактика", "Шутер"],
     price: 982,
-    id: 5,
+    id: Math.random(),
     description:
       "Tom Clancy's Rainbow Six Осада – это продолжение нашумевшего шутера от первого лица, разработанного студией Ubisoft Montreal.",
   },
@@ -57,7 +59,7 @@ const games = [
     genres: ["Паркур", "РПГ", "Открытый мир"],
     video: "https://www.youtube.com/embed/ssrNcwxALS4",
     price: 2863,
-    id: 6,
+    id: Math.random(),
     description:
       "Assassins Creed Valhalla — мультиплатформенная компьютерная игра в жанре action/RPG, разработанная студией Ubisoft Montreal под издательством компании Ubisoft. Является двенадцатой игрой в серии игр Assassins Creed.",
   },
@@ -67,7 +69,7 @@ const games = [
     genres: ["Шутер", "Экшен"],
     video: "https://www.youtube.com/embed/PGh3tm78aoM",
     price: 3063,
-    id: 7,
+    id: Math.random(),
     description:
       "В Call of Duty®: Black Ops III используется новая цепная система плавного движения, позволяющая игрокам ловко перемещаться по окружающей среде, используя контролируемые прыжки, силовые скольжения, бег по стенам, плавание и быстрое маскирование во множестве способов.",
   },
@@ -76,10 +78,20 @@ const games = [
     title: "Fall Guys",
     genres: ["Симулятор", "Мультиплейер"],
     video: "https://www.youtube.com/embed/Wj3dUvGLjNQ",
-    price: 3063,
-    id: 8,
+    price: 2503,
+    id: Math.random(),
     description:
       "Fall Guys (ранее Fall Guys: Ultimate Knockout) — платформер в жанре королевской битвы, разработанный Mediatonic. В игре участвуют до 60 игроков, которые управляют мармеладными существами и соревнуются друг с другом в серии случайно выбранных мини-игр, таких как полоса препятствий или теги",
+  },
+  {
+    image: "/game-covers/csgo.png",
+    title: "CS GO",
+    genres: ["Шутер", "Мультиплейер"],
+    video: "https://www.youtube.com/embed/edYCtaNueQY",
+    price: 3903,
+    id: Math.random(),
+    description:
+      "Counter-Strike: Global Offensive — многопользовательский шутер от первого лица 2012 года, разработанный Valve и Hidden Path Entertainment. Это четвертая игра в серии Counter-Strike.",
   },
 ];
 export const HomePage = () => {
