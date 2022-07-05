5d6a112f5be0ef20d85a239553bafc12759d60ae
Your Name <mustafa6143170@gmail.com>
2022-07-06 01:25:39 +0500

    push

--- tesla-clone/layout/Navbar/Navbar.jsx
+++ tesla-clone/layout/Navbar/Navbar.jsx
@@ -1,72 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbar.module.scss";
 import Link from "next/link";
-import Image from "next/image";
+import { SideBar } from "./SideBar";
+import CloseIcon from "@mui/icons-material/Close";
 const Navbar = () => {
+  const [sideBar, setsideBar] = useState(false);
+  const showSideBar = () => {
+    setsideBar((prev) => {
+      return !prev;
+    });
+  };
   return (
-    <nav className={styles.navbar}>
-      <Link href="/">
-        <a>
-          <img
-            className={styles.teslaLogo}
-            src="/images/logo.svg"
-            alt="logo"
-            loading="lazy"
-          />
-        </a>
-      </Link>
-      {/* NAV LINK */}
-      <ul className={styles.navbarNav}>
-        <li className={styles.items}>
-          <a href="/" className={styles.itemsLink}>
-            Model S
+    <>
+      <nav className={styles.navbar}>
+        <Link href="/">
+          <a>
+            <img
+              className={styles.teslaLogo}
+              src="/images/logo.svg"
+              alt="logo"
+              loading="lazy"
+            />
           </a>
-        </li>
-        <li className={styles.items}>
-          <a href="/" className={styles.itemsLink}>
-            Model 3
-          </a>
-        </li>
-        <li className={styles.items}>
-          <a href="/" className={styles.itemsLink}>
-            Model X
-          </a>
-        </li>
-        <li className={styles.items}>
-          <a href="/" className={styles.itemsLink}>
-            Model Y
-          </a>
-        </li>
-        <li className={styles.items}>
-          <a href="/" className={styles.itemsLink}>
-            Solar Roof
-          </a>
-        </li>
-        <li className={styles.items}>
-          <a href="/" className={styles.itemsLink}>
-            Solar Panels
-          </a>
-        </li>
-      </ul>
-      {/* NAV MENU */}
-      <div className={styles.menu}>
-        <li className={styles.items}>
-          <a href="/" className={styles.itemsLink}>
-            Solar Panels
-          </a>
-        </li>
-        <li className={styles.items}>
-          <a href="/" className={styles.itemsLink}>
-            Solar Panels
-          </a>
-        </li>
-        <li className={styles.items}>
-          <a href="/" className={styles.itemsLink}>
-            Solar Panels
-          </a>
-        </li>
-      </div>
-    </nav>
+        </Link>
+        {/* NAV LINK */}
+        <ul className={styles.navbarNav}>
+          <li className={styles.items}>
+            <a href="/" className={styles.itemsLink}>
+              Model S
+            </a>
+          </li>
+          <li className={styles.items}>
+            <a href="/" className={styles.itemsLink}>
+              Model 3
+            </a>
+          </li>
+          <li className={styles.items}>
+            <a href="/" className={styles.itemsLink}>
+              Model X
+            </a>
+          </li>
+          <li className={styles.items}>
+            <a href="/" className={styles.itemsLink}>
+              Model Y
+            </a>
+          </li>
+          <li className={styles.items}>
+            <a href="/" className={styles.itemsLink}>
+              Solar Roof
+            </a>
+          </li>
+          <li className={styles.items}>
+            <a href="/" className={styles.itemsLink}>
+              Solar Panels
+            </a>
+          </li>
+        </ul>
+        {/* NAV MENU */}
+        <div className={styles.menu}>
+          <li className={styles.items}>
+            <a href="/" className={styles.itemsLink}>
+              Shop
+            </a>
+          </li>
+          <li className={styles.items}>
+            <a href="/" className={styles.itemsLink}>
+              Account
+            </a>
+          </li>
+          <li className={styles.items}>
+            <a className={styles.itemsLink} onClick={showSideBar}>
+              Menu
+            </a>
+          </li>
+        </div>
+        <div
+          className={
+            sideBar
+              ? styles["sideMenu"] + " " + styles["active"]
+              : styles["sideMenu"]
+          }
+        >
+          <CloseIcon className={styles.closeIcon} onClick={showSideBar} />
+          <ul className={styles.sideBarNav}>
+            {SideBar.map((item, key) => {
+              return (
+                <li key={key} className={styles.hamburgerLinks}>
+                  <Link href={item.path}>{item.title}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+      {/* SIDE BAR MENU */}
+    </>
   );
 };
 
