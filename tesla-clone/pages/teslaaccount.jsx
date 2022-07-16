8d9a3182207538310ac5f749e8e5d6fa8f431960
Your Name <mustafa6143170@gmail.com>
2022-07-16 15:38:22 +0500

    push

--- tesla-clone/pages/teslaaccount.jsx
+++ tesla-clone/pages/teslaaccount.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import { useAuth } from "../context/AuthUserContext";
+import styles from "../styles/teslaaccount.module.scss";
+import { style } from "@mui/system";
 const teslaaccount = () => {
   const { loading, authUser, signOut } = useAuth();
   const router = useRouter();
-  useEffect(() => {
-    if (!authUser && !loading) {
-      router.push("/signUp");
-    }
-  }, [authUser, loading]);
+  // useEffect(() => {
+  //   if (!authUser && !loading) {
+  //     router.push("/signUp");
+  //   }
+  // }, [authUser, loading]);
 
   return (
     <>
@@ -24,7 +27,21 @@ const teslaaccount = () => {
               content="width=device-width, initial-scale=1.0"
             />
           </Head>
-          <h1>Tesla Account</h1>
+          {/* <Navbar /> */}
+          <div className={styles.logInfo}>
+            {authUser && (
+              <p>
+                You`re logged as {authUser.email}`s <b>email</b>
+              </p>
+            )}
+            <button
+              onClick={console.log(() => signOut)}
+              className={styles.logOutBtn}
+            >
+              <ExitToAppIcon className={styles.btnIcon} />
+              Sign Out
+            </button>
+          </div>
         </>
       )}
     </>
