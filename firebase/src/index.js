b1103e099beeb744d980dd116a2adf5d31f46933
Your Name <mustafa6143170@gmail.com>
2022-07-27 20:50:09 +0500

    push

--- firebase/src/index.js
+++ firebase/src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 import { store } from "./store";
 import "./firebase";
-import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import "./styles.css";
 
 const container = document.getElementById("root");
