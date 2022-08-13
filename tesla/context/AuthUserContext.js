1a9791f0004e284b89ee04a543036b12610de691
Your Name <mustafa6143170@gmail.com>
2022-08-14 01:27:45 +0500

    push

--- tesla/context/AuthUserContext.js
+++ tesla/context/AuthUserContext.js
@@ -9,7 +9,7 @@ const authUserContext = createContext({
   signOut: () => {},
 });
 
-export function AuthUserProvider({ children }) {
+export default function AuthUserProvider({ children }) {
   const auth = useFirebaseAuth();
   return (
     <authUserContext.Provider value={auth}>{children}</authUserContext.Provider>
