bf68478c14b06ab20e47d1b42125a49a751352ae
Your Name <mustafa6143170@gmail.com>
2022-08-14 01:21:30 +0500

    push

--- tesla/context/AuthUserContext.js
+++ tesla/context/AuthUserContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import {useFirebaseAuth} from "../lib/useFirebaseAuth";
+import useFirebaseAuth from "../lib/useFirebaseAuth";
 
 const authUserContext = createContext({
   authUser: null,
