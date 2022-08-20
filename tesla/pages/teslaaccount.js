a43c035b2cbeda5360be503bca77142869490dda
Mustafo Yakubov <mustafa6143170@gmail.com>
2022-08-21 01:08:35 +0500

    push

--- tesla/pages/teslaaccount.js
+++ tesla/pages/teslaaccount.js
@@ -6,7 +6,7 @@ import styles from '../styles/Account.module.css'
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import AccountNavbar from '../components/AccountNavbar';
 
-const TeslaAccount = () => {
+const LoggedIn = () => {
     const { authUser, loading, signOut } = useAuth()
     const router = useRouter()
 
@@ -56,4 +56,4 @@ const TeslaAccount = () => {
     );
 };
 
-export default TeslaAccount;
+export default LoggedIn;
